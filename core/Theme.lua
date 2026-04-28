a6b4edeb741a2b8d591fe68db469a5607f54bbf4
XyiBobra5242 <ruslan.kojonov.kg15@gmail.com>
2026-04-28 19:14:30 +0300

    Create Theme.lua

---
+++ core/Theme.lua
@@ -0,0 +1,11 @@
+getgenv().CrystalLevPlatform.Theme = {
+    Accent = Color3.fromRGB(0, 255, 140),
+    BG = Color3.fromRGB(18, 18, 18),
+    BG2 = Color3.fromRGB(28, 28, 28),
+    BG3 = Color3.fromRGB(38, 38, 38),
+    Text = Color3.fromRGB(255, 255, 255),
+    Text2 = Color3.fromRGB(160, 160, 160),
+    Red = Color3.fromRGB(255, 60, 60),
+    Orange = Color3.fromRGB(255, 160, 40),
+    Blue = Color3.fromRGB(80, 140, 255),
+}
